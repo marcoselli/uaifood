b7f8e861fcdc302f49cba1624cefaad352149b34
Guilherme Sousa <guilherme.sousa@fpf.org.br>
2024-11-05 09:50:11 -0300

    Ajuste no Dockerfile para usar o gradle da imagem e ajueste no DTO de produtos para corrigir problema na img_url

--- src/main/kotlin/br/edu/uaifood/ports/inbound/api/product/dto/UpsertProductRequest.kt
+++ src/main/kotlin/br/edu/uaifood/ports/inbound/api/product/dto/UpsertProductRequest.kt
@@ -1,9 +1,12 @@
 package br.edu.uaifood.ports.inbound.api.product.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class UpsertProductRequest(
     val name: String,
     val description: String,
     val price: Double,
     val category: String,
+    @JsonProperty("image_url")
     val imageUrl: String
 )
